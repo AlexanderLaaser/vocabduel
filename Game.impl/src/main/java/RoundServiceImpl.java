5085e71ec8e62e74e3af13bcb812e0b0666d7425
s0564451 <s0564451@htw-berlin.de>
2021-10-31 17:37:37 +0100

    implement RoundSeviceTest and GameStatisticServiceTest

--- Game.impl/src/main/java/RoundServiceImpl.java
+++ Game.impl/src/main/java/RoundServiceImpl.java
@@ -1,17 +1,31 @@
 import game.inter.RoundService;
-import vocabmanagement.inter.VocabListService;
 
 public class RoundServiceImpl implements RoundService {
 
-    private VocabListService vocabListService;
 
+    //asks the User for his Answer
     @Override
-    public void askForGuess(){
+    public int askForGuess(){
 
+        return 0;
     }
 
     @Override
-    public int calculateRoundResults(int winningUser){
-        return winningUser;
+    public int calculateRoundResults(int winningUser) {
+        return 0;
+    }
+
+    //calculates the Winner of the Round and gives back a Integer
+    //0=tie 1= User1 wins 2= User2 wins
+    @Override
+    public int calculateRoundResults(){
+
+
+        return 0;
+    }
+
+    @Override
+    public void createRound(){
+
     }
 }
--- Game.inter/src/main/java/game/inter/RoundService.java
+++ Game.inter/src/main/java/game/inter/RoundService.java
@@ -1,21 +1,24 @@
 package game.inter;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-
 public interface RoundService {
 
     /**
      * asks the User for his Answer
+     * @return an integer contains the answer of the user
      */
-    void askForGuess();
+    int askForGuess();
 
     /**
      * calculates the Winner of the Round and gives back a Integer
      * 0=tie 1= User1 wins 2= User2 wins
-     * @param winningUser
-     * @return
+     * @param winningUser is the winning user of the round
+     * @return integer contains the number of the winning Round User
      */
     int calculateRoundResults(int winningUser);
+
+    //calculates the Winner of the Round and gives back a Integer
+    //0=tie 1= User1 wins 2= User2 wins
+    int calculateRoundResults();
+
+    void createRound();
 }
