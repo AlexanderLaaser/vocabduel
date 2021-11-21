6140a0ab4072629df687a1db4820821468d1db59
Alexander Laaser <45990752+AlexanderLaaser@users.noreply.github.com>
2021-11-21 16:53:50 +0100

    VocabLogik + File Import

---
+++ configuration_manager/src/main/java/de/htwberlin/configuration_manager/ConfigurationSpringImpl.java
@@ -0,0 +1,24 @@
+package de.htwberlin.configuration_manager;
+
+import de.htwberlin.game_ui.impl.GameUiControllerImpl;
+import de.htwberlin.game_ui.inter.GameUiController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.naming.InvalidNameException;
+import java.io.IOException;
+
+
+public class ConfigurationSpringImpl {
+
+    private static ConfigurableApplicationContext context = new AnnotationConfigApplicationContext("de.htwberlin");
+
+    public static void main(String[] args) throws IOException, InvalidNameException, de.htwberlin.vocabmanagement.inter.InvalidNameException {
+        GameUiController ui = context.getBean(GameUiController.class);
+          ui.run();
+
+          //Test
+    }
+
+}
